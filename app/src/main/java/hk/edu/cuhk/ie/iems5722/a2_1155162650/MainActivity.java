73d3ac56ab82484c4c660e19c9aa3435e1be325a
99xiewilliam <35141949+99xiewilliam@users.noreply.github.com>
2021-10-22 20:25:01 +0800

    submit change

--- app/src/main/java/hk/edu/cuhk/ie/iems5722/a2_1155162650/MainActivity.java
+++ app/src/main/java/hk/edu/cuhk/ie/iems5722/a2_1155162650/MainActivity.java
@@ -1,11 +1,18 @@
 package hk.edu.cuhk.ie.iems5722.a2_1155162650;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -24,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Context context = this;
+//        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+//        setSupportActionBar(myToolbar);
+
 
         new HttpTask() {
 
@@ -40,6 +50,18 @@ public class MainActivity extends AppCompatActivity {
                     }
                     listView = findViewById(R.id.chatrooms);
                     listView.setAdapter(new ArrayAdapter<String>(context, R.layout.activity_list_view, R.id.testView, list));
+
+                    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                            Object item = listView.getAdapter().getItem(i);
+                            Intent it = new Intent(context, ChatActivity.class);
+
+                            startActivity(it);
+
+                            //Toast.makeText(context, item.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -55,5 +77,7 @@ public class MainActivity extends AppCompatActivity {
 //        listView.setAdapter(new ArrayAdapter<String>(this, R.layout.activity_list_view, R.id.testView, list));
 
     }
+
+
 }
 
